a07ac462f65f8f9de747e339e61e56d26cd20ed7
saikrishna k <saikrishnak1997@gmail.com>
2017-02-25 14:45:59 +0530

    add

--- p.c
+++ p.c
@@ -1,13 +1,26 @@
 #include<stdio.h>
 void main()
 	{
-		int c;
+		int c,n1,n2,sum;
 		do 
 		{
 			printf("calculator\n");
 			printf("1.add 2.sub 3.mul 4.div 5.exit\n");
 			printf("enter choice");
 			scanf("%d",&c);
+			switch(c)
+			{
+			case 1 : printf("enter the 2 nos:");
+					scanf("%d",&n1);
+					scanf("%d",&n2);
+					sum=n1+n2;
+					printf("sum=%d\n",sum);
+					break;
+					
+
+			}
+
 		}
 		while(c!=5);
 	}
+
